fa5347582df4bfa0533044790bb98e42cd624fb2
SetsunaXpro <showarider812@gmail.com>
2025-06-02 11:41:57 +0700

    Java Chess Java FX

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        WebView webView = new WebView();
+        webView.getEngine().load("file:///C:/Users/showa/javapchess/test.html");
+        Scene scene = new Scene(webView, 800, 600);
+        stage.setScene(scene);
+        stage.setTitle("java gui");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
